8737a9a021aae832dbb3704dcc4feba9fa2bc529
vik-him <viktosiyus@inbox.lv>
2018-11-27 16:02:56 +0200

    tilemap

--- src/main/java/TileMap/TileMap.java
+++ src/main/java/TileMap/TileMap.java
@@ -98,6 +98,11 @@ public class TileMap {
             width = numCols * tileSize;
             height = numRows * tileSize;
 
+            xmin = GamePanel.WIDTH - width;
+            xmax = 0;
+            ymin = GamePanel.HEIGHT - height;
+            ymax = 0;
+
             String delims = "\\s+";
             for(int row = 0; row < numRows; row++) {
                 String line = br.readLine();
