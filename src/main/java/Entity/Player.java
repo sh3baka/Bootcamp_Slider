6ee1ffc39abdd249825db29f309410ce20f6e99a
vik-him <viktosiyus@inbox.lv>
2018-11-27 13:42:43 +0200

    Animation

---
+++ src/main/java/Entity/Player.java
@@ -0,0 +1,10 @@
+package Entity;
+
+public class Player extends MapObject {
+
+    //player stuff
+    private int health;
+    private int maxHealth;
+    private int fire;
+    private int maxFire;
+}
