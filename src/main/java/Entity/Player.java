3eb09eed76c19fd3fff083ffa96531efea11b09c
vik-him <viktosiyus@inbox.lv>
2018-11-27 15:37:31 +0200

    Player

--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -16,7 +16,7 @@ public class Player extends MapObject {
     private int maxFire;
     private boolean dead;
     private boolean flinching;
-    private long flinchTime;
+    private long flinchTimer;
 
     //fireball
     private boolean firing;
@@ -35,7 +35,7 @@ public class Player extends MapObject {
     // animations
     private ArrayList<BufferedImage[]> sprites;
     private final int[] numFrames = {
-        2, 8, 1, 2, 4, 2, 5
+            2, 8, 1, 2, 4, 2, 5
     };
 
     //animation actions
@@ -50,6 +50,263 @@ public class Player extends MapObject {
     public Player(TileMap tm) {
 
         super(tm);
+
+        width = 30;
+        height = 30;
+        cwidth = 20;
+        cheight = 20;
+
+        moveSpeed = 0.3;
+        maxSpeed = 1.6;
+        stopSpeed = 0.4;
+        fallSpeed = 4.0;
+        jumpStart = -4.8;
+        stopJumpSpeed = 0.3;
+
+        facingRight = true;
+
+        health = maxHealth = 5;
+        fire = maxFire = 2500;
+
+        fireCost = 200;
+        fireBallDamage = 5;
+        //fireBalls = new ArrayList<FireBall>();
+
+
+        scratchDamage = 8;
+        scratchRange = 40;
+
+        //load sprites
+        try {
+            BufferedImage spritesheet = ImageIO.read(
+                    getClass().getResourceAsStream(
+                            "/Sprites/Player/playersprites.gif"
+                    )
+            );
+
+            for (int i = 0; i < 7; i++) {
+
+                BufferedImage[] bi =
+                        new BufferedImage[numFrames[i]];
+
+                for (int j = 0; j < numFrames[i]; j++) {
+
+                    if (i != 6) {
+                        bi[j] = spritesheet.getSubimage( //res
+                                j * width,
+                                i * height,
+                                width,
+                                height
+                        );
+
+                    } else {
+                        bi[j] = spritesheet.getSubimage( //res
+                                j * width * 2,
+                                i * height,
+                                width,
+                                height
+                        );
+                    }
+
+                }
+
+                sprites.add(bi);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getFire() {
+        return fire;
+    }
+
+    public int getMaxFire() {
+        return maxFire;
+    }
+
+    public void setFiring() {
+        firing = true;
+    }
+
+    public void setScratching() {
+        scratching = true;
+    }
+
+    public void setGliding(boolean b) {
+        gliding = b;
+    }
+
+    private void getNextPosition() {
+
+        //movement
+        if (left) {
+            dx -= moveSpeed;
+            if (dx < -maxSpeed) {
+                dx = -maxSpeed;
+            }
+        } else if (right) {
+            dx += moveSpeed;
+            if (dx > maxSpeed) {
+                dx = maxSpeed;
+            }
+        } else {
+            if (dx > 0) {
+                dx -= stopSpeed;
+                if (dx < 0) {
+                    dx = 0;
+                }
+            } else if (dx < 0) {
+                dx += stopSpeed;
+                if (dx > 0) {
+                    dx = 0;
+                }
+            }
+        }
+
+        //cannot move while attacking, except in air
+        if(
+                (currentAction == SCRATCHING || currentAction == FIREBALL)) &&
+        !(jumping || falling)) {
+            dx = 0;
+        }
+
+        //jumping
+        if(jumping && !falling) {
+            dy = jumpStart;
+            falling = true;
+    }
+        //falling
+        if (falling) {
+
+            if(dy > 0 &&gliding) dy += fallSpeed * 0.1;
+            else dy += fallSpeed;
+
+            if (dy > 0) jumping = false;
+            if (dy < 0 && !jumping) dy += stopJumpSpeed;
+            if(dy > maxFallSpeed) dy = maxFallSpeed;
+
+        }
+    }
+
+    public void update() {
+
+        //update position
+        getNextPosition();
+        checkTileMapCollision();
+        setPosition(xtemp, ytemp);
+
+        //set animation
+        if (scratching) {
+            if (currentAction != SCRATCHING) {
+                currentAction = SCRATCHING;
+                animation.setFrames(sprites.get(SCRATCHING));
+                animation.setDelay(50);
+                width = 60;
+            }
+        } else if (firing) {
+            if (currentAction != FIREBALL) {
+                currentAction = FIREBALL;
+                animation.setFrames(sprites.get(FIREBALL));
+                animation.setDelay(100);
+                width = 30;
+            }
+        }
+    else if(dy >0)
+
+    {
+        if (gliding) {
+            if (currentAction != GLIDING) {
+                currentAction = GLIDING;
+                animation.setFrames(sprites.get(GLIDING));
+                animation.setDelay(100);
+                width = 30;
+            }
+        } else if (currentAction != FALLING) {
+            currentAction = FALLING;
+            animation.setFrames(sprites.get(FALLING));
+            animation.setDelay(100);
+            width = 30;
+        }
+    }
+    else if(dy< 0)
+
+    {
+        if (currentAction != JUMPING) {
+            currentAction = JUMPING;
+            animation.setFrames(sprites.get(JUMPING));
+            animation.setDelay(-1);
+            width = 30;
+        }
+    }
+    else if(left ||right)
+
+    {
+        if (currentAction != WALKING) {
+            currentAction = WALKING;
+            animation.setFrames(sprites.get(WALKING));
+            animation.setDelay(40);
+            width = 30;
+        }
+    }
+    else
+
+    {
+        if (currentAction != IDLE) {
+            currentAction = IDLE;
+            animation.setFrames(sprites.get(IDLE));
+            animation.setDelay(400);
+            width = 30;
+        }
+    }
+    animation.update();
+
+    //set direction
+    if(currentAction !=SCRATCHING &&currentAction !=FIREBALL)
+            if(right)facingRight = true;
+             if(left)facingRight = false;
+        }
+
+    }
+
+    public void draw(Graphics2D g){
+
+        setMapPosition();
+
+        //draw player
+        if(flinching) {
+            long elapsed =
+                    (System.nanoTime() - flinchTimer) / 1000000;
+            if(elapsed / 100 % 2 == 0) {
+                return;
+            }
+        }
+        if(facingRight) {
+            g.drawImage(animation.getImage(),
+                    (int)(x + xmap - width / 2),
+                    (int)(y + ymap - height / 2),
+                    null
+            );
+        }
+        else {
+            g.drawImage(
+                    animation.getImage(),
+                    (int)(x + xmap -width / 2 + width),
+                    (int)(y + ymap - height / 2),
+                    -width,
+                    height,
+                    null
+
+            );
+        }
+        }
 }
