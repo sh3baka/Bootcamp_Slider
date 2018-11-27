d1cb3f0ff71a7fdfa68409298129e41cd0edc5d8
vik-him <viktosiyus@inbox.lv>
2018-11-27 12:35:37 +0200

    Tile

--- src/main/java/TileMap/TileMap.java
+++ src/main/java/TileMap/TileMap.java
@@ -83,6 +83,106 @@ public class TileMap {
         }
     }
 
-    public void loadMap(String s) {}
+    public void loadMap(String s) { //resources
+
+        try {
+
+            InputStream in = getClass().getResourceAsStream(s);
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(in)
+            );
+
+            numCols = Integer.parseInt(br.readLine());
+            numRows = Integer.parseInt(br.readLine());
+            map = new int[numRows][numCols];
+            width = numCols * tileSize;
+            height = numRows * tileSize;
+
+            String delims = "\\s+";
+            for(int row = 0; row < numRows; row++) {
+                String line = br.readLine();
+                String[] tokens = line.split(delims);
+                for (int col = 0; col < numCols; col++) {
+                    map[row][col] = Integer.parseInt(tokens[col]);
+                }
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public int getTileSize() {return tileSize; }
+    public int getx() { return (int)x; }
+    public int gety() { return  (int)y; }
+    public int getWidth() {return width; }
+    public int getHeight() { return height; }
+
+    public int getType(int row, int col) {
+        int rc = map[row][col]; //resources
+        int r = rc / numTilesAcross;
+        int c = rc % numTilesAcross;
+        return tiles[r][c].getType();
+
+    }
+
+    public void setPosition(double x, double y) {
+
+        this.x += (x - this.x) * tween;
+        this.y += (y - this.y) * tween;
+
+        fixBounds();
+
+        colOffset = (int)-this.x / tileSize;
+        rowOffset = (int)-this.y / tileSize;
+
+    }
+
+    private void fixBounds() {
+        if(x < xmin) x = xmin;
+        if(x < ymin) y = ymin;
+        if(x > xmax) x = xmax;
+        if(x > ymax) y = ymax;
+    }
+
+    public  void draw(Graphics2D g) {
+
+        for(
+                int row = rowOffset;
+                row < rowOffset + numRowsToDraw;
+                row++) {
+
+            if(row >= numRows) break;
+
+            for (
+                    int col = colOffset;
+                    col < colOffset + numNumColsToDraw;
+                    col++) {
+
+                if(col >= numCols) break;
+
+                if(map[row][col] == 0) continue; //res
+
+                int rc = map[row][col];
+                int r = rc / numTilesAcross;
+                int c = rc % numTilesAcross;
+
+                g.drawImage(
+                        tiles[r][c].getImage(),
+                        (int)x + col * tileSize,
+                        (int)y + row * tileSize,
+                        null
+                );
+
+
+
+            }
+
+
+        }
+
+    }
 
 }
