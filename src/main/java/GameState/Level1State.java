40dcb378957684a14380c952a4ae010e968724c4
vik-him <viktosiyus@inbox.lv>
2018-11-27 16:00:04 +0200

    level

--- src/main/java/GameState/Level1State.java
+++ src/main/java/GameState/Level1State.java
@@ -1,9 +1,11 @@
 package GameState;
 
 import Entity.*;
+import Main.GamePanel;
 import TileMap.*;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Level1State extends GameState {
 
@@ -23,12 +25,17 @@ public class Level1State extends GameState {
         tileMap.loadTiles("/Tiles/grass.png");
 
         player = new Player(tileMap);
+        player.setPosition(100, 100);
 
     }
     public void update() {
 
         //update player
         player.update();
+        tileMap.setPosition(
+                GamePanel.WIDTH / 2 - player.getx(),
+                GamePanel.HEIGHT / 2 - player.gety()
+        );
 
     }
 
@@ -44,7 +51,24 @@ public class Level1State extends GameState {
         player.draw(g
         );
     }
-    public void keyPressed(int k) {}
-    public void keyReleased(int k) {}
+    public void keyPressed(int k) {
+        if(k == KeyEvent.VK_LEFT) player.setLeft(true);
+        if(k == KeyEvent.VK_RIGHT) player.setRight(true);
+        if(k == KeyEvent.VK_UP) player.setUp(true);
+        if(k == KeyEvent.VK_DOWN) player.setDown(true);
+        if(k == KeyEvent.VK_W) player.setJumping(true);
+        if(k == KeyEvent.VK_E) player.setGliding(true);
+        if(k == KeyEvent.VK_R) player.setScratching();
+        if(k == KeyEvent.VK_F) player.setFiring();
+
+    }
+    public void keyReleased(int k) {
+        if(k == KeyEvent.VK_LEFT) player.setLeft(false);
+        if(k == KeyEvent.VK_RIGHT) player.setRight(false);
+        if(k == KeyEvent.VK_UP) player.setUp(false);
+        if(k == KeyEvent.VK_DOWN) player.setDown(false);
+        if(k == KeyEvent.VK_W) player.setJumping(false);
+        if(k == KeyEvent.VK_E) player.setGliding(false);
+    }
 
 }
