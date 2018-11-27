d21040e0bdc6c159c443d823812836e6aa9ece7a
vik-him <viktosiyus@inbox.lv>
2018-11-27 15:44:48 +0200

    level

--- src/main/java/GameState/Level1State.java
+++ src/main/java/GameState/Level1State.java
@@ -1,12 +1,16 @@
 package GameState;
 
-import TileMap.TileMap;
+import Entity.*;
+import TileMap.*;
 
 import java.awt.*;
 
 public class Level1State extends GameState {
 
     private TileMap tileMap;
+    private Background bg;
+
+    private Player player;
 
     public Level1State(GameStateManager gsm) {
         this.gsm = gsm;
@@ -18,9 +22,27 @@ public class Level1State extends GameState {
         tileMap = new TileMap(30);
         tileMap.loadTiles("/Tiles/grass.png");
 
+        player = new Player(tileMap);
+
     }
-    public void update() {}
-    public void draw(Graphics2D g) {}
+    public void update() {
+
+        //update player
+        player.update();
+
+    }
+
+    public void draw(Graphics2D g) {
+
+        //draw bg
+        bg.draw(g);
+
+        //draw tilemap
+        tileMap.draw(g);
+
+        //draw player
+        player.draw(g);
+    }
     public void keyPressed(int k) {}
     public void keyReleased(int k) {}
 
