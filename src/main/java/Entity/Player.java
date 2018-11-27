505e01d8a710de5db5420b3233fb2ccb60dd0957
vik-him <viktosiyus@inbox.lv>
2018-11-27 13:54:33 +0200

    Player

--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -1,5 +1,12 @@
 package Entity;
 
+import TileMap.*;
+
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Player extends MapObject {
 
     //player stuff
@@ -7,4 +14,28 @@ public class Player extends MapObject {
     private int maxHealth;
     private int fire;
     private int maxFire;
+    private boolean dead;
+    private boolean flinching;
+    private long flinchTime;
+
+    //fireball
+    private boolean firing;
+    private int fireCost;
+    private int fireBallDamage;
+    //private ArrayList<FireBall> fireBalls;
+
+    //scratch
+    private boolean scratching;
+    private int scratchDamage;
+    private int scratchRange;
+
+    //glinding
+    private boolean gliding;
+
+    // animations
+    private ArrayList<BufferedImage[]> sprites;
+
+    //animation actions
+
+
 }
