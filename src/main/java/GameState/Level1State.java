2f3e04001a8270fce647399aadc99cb5b5f1d178
Ilmars Zeps <ilmars.zeps@gmail.com>
2018-11-28 12:47:34 +0200

    Merge branch 'master' of C:\Users\ilmar\Desktop\LookingForMe\Bootcamp_Slider with conflicts.

--- src/main/java/GameState/Level1State.java
+++ src/main/java/GameState/Level1State.java
@@ -1,16 +1,14 @@
 package GameState;
 
-import Entity.Player;
 import Main.GamePanel;
 import TileMap.TileMap;
-
+import TileMap.Background;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
 public class Level1State extends GameState {
 
     private TileMap tileMap;
-    Player player;
+    private Background bg;
 
     public Level1State(GameStateManager gsm) {
         this.gsm = gsm;
@@ -19,33 +17,19 @@ public class Level1State extends GameState {
 
     public void init() {
         tileMap = new TileMap(30);
-        tileMap.loadTiles("/tiles_lvl1_v2.png");
-        tileMap.loadMap("/TestMap.csv");
-        player = new Player(tileMap);
-        player.setPosition(200,50);
-    }
-    public void update() {
-        player.update();
+        tileMap.loadTiles("/tiles_lvl1_v3.png");
+        tileMap.loadMap("/testMap.map");
+        bg = new Background("/bg.png",0.1);
 
 
     }
+    public void update() {}
     public void draw(Graphics2D g) {
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
         tileMap.draw(g);
-        player.draw(g);
     }
-    public void keyPressed(int k) {
-        if(k == KeyEvent.VK_LEFT) player.setLeft(true);
-        if(k == KeyEvent.VK_RIGHT) player.setRight(true);
-        if(k == KeyEvent.VK_UP) player.setUp(true);
-        if(k == KeyEvent.VK_DOWN) player.setDown(true);
-    }
-    public void keyReleased(int k) {
-        if(k == KeyEvent.VK_LEFT) player.setLeft(false);
-        if(k == KeyEvent.VK_RIGHT) player.setRight(false);
-        if(k == KeyEvent.VK_UP) player.setUp(false);
-        if(k == KeyEvent.VK_DOWN) player.setDown(false);
-    }
+    public void keyPressed(int k) {}
+    public void keyReleased(int k) {}
 
 }
