14b4f9b5559bae896b629a45b9eb8035e44c0b81
vik-him <viktosiyus@inbox.lv>
2018-11-28 14:55:39 +0200

    HUD

---
+++ src/main/java/Entity/HUD.java
@@ -0,0 +1,38 @@
+package Entity;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class HUD {
+
+    private Player player;
+
+    private BufferedImage image;
+    private Font font;
+
+    public HUD(Player p) {
+        player = p;
+        try{
+            image = ImageIO.read(
+                    getClass().getResourceAsStream(
+                            "/HUD/hud_p3.png"
+
+                    )
+            );
+            font = new Font("Arial", Font.PLAIN,14);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void draw(Graphics2D g) {
+
+        g.drawImage(image, 0, 20, null);
+        g.setFont(font);
+        //g.drawString();
+    }
+
+}
