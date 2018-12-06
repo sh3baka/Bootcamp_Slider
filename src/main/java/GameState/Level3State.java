bc19b4cfc3576e0f19985bda5b2f706473dbed38
vik-him <viktosiyus@inbox.lv>
2018-12-06 12:41:17 +0200

    populate

--- src/main/java/GameState/Level3State.java
+++ src/main/java/GameState/Level3State.java
@@ -160,6 +160,10 @@ public class Level3State extends GameState {
                 new Point(1260,100),
                 new Point(1280,100),
                 new Point(1610, 200),
+                new Point(480,200),
+                new Point(2260,100),
+                new Point(2260,110),
+                new Point(2740,60)
 
         };
         for (Point point : coinPoints) {
@@ -207,7 +211,59 @@ public class Level3State extends GameState {
         Point[] coinPoints = new Point[]{
                 new Point(340, 100),
                 new Point(360, 100),
-                new Point(380, 100)
+                new Point(380, 100),
+                new Point(3220,200),
+                new Point(3240,200),
+                new Point(3260,200)
+        };
+        for (Point point : coinPoints) {
+            c = new GoldCoin(tileMap);
+            c.setPosition(point.x, point.y);
+            goldCoins.add(c);
+        }
+    }
+    private void populateThings3() {
+
+        Slugger s;
+        Fly fly;
+        Slime slime;
+
+        LinkedList<Point> sluggerPoints = new LinkedList<Point>();
+        sluggerPoints.add(new Point(4130, 180));
+
+        for (Point point : sluggerPoints) {
+            s = new Slugger(tileMap);
+            s.setPosition(point.x, point.y);
+            slugs.add(s);
+        }
+
+        LinkedList<Point> flyPoints = new LinkedList<Point>();
+        flyPoints.add(new Point(4060, 170));
+
+        for (Point point : flyPoints) {
+            fly = new Fly(tileMap);
+            fly.setPosition(point.x, point.y);
+            flys.add(fly);
+        }
+
+        LinkedList<Point> slimePoints = new LinkedList<Point>();
+        slimePoints.add(new Point(220, 100));
+
+        for (Point point : slimePoints) {
+            slime = new Slime(tileMap);
+            slime.setPosition(point.x, point.y);
+            slimes.add(slime);
+        }
+        //coins
+        goldCoins = new ArrayList<Collectible>();
+        GoldCoin c;
+        Point[] coinPoints = new Point[]{
+                new Point(4060, 170),
+                new Point(4080, 170),
+                new Point(4200, 110),
+                new Point(4220,100),
+                new Point(4390,140),
+                new Point(4310,100)
         };
         for (Point point : coinPoints) {
             c = new GoldCoin(tileMap);
