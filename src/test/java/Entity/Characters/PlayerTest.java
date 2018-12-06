9cc230817a493121280578dca796eb840919513e
Konstantins Baranovskis <k.baranovskis@gmail.com>
2018-12-06 12:27:34 +0200

    Player Tests

--- src/test/java/Entity/Characters/PlayerTest.java
+++ src/test/java/Entity/Characters/PlayerTest.java
@@ -1,14 +1,23 @@
 package Entity.Characters;
 
+import Entity.Collectible.Collectible;
+import Entity.Collectible.GoldCoin;
+import Entity.Collectible.YellowKey;
 import TileMap.TileMap;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class PlayerTest {
 
     private static final double DELTA = 1e-15;
 
+
+    ArrayList<Enemy> enemies;
 
     TileMap tm = new TileMap(30);
     Player player = new Player(tm);
@@ -59,14 +68,14 @@ public class PlayerTest {
     public void getKey() {
         player.setKey(true);
 
-        assertEquals(true, player.getKey());
+        assertTrue(player.getKey());
     }
 
     @Test
     public void setKey() {
         player.setKey(true);
 
-        assertEquals(true, player.getKey());
+        assertTrue(player.getKey());
     }
 
     @Test
@@ -80,38 +89,84 @@ public class PlayerTest {
     public void setDead() {
         player.setDead();
 
-        assertEquals(true, player.isDead());
+        assertTrue(player.isDead());
     }
 
-//    @Test
-//    public void checkDead() {
-//    }
-//
-//    @Test
-//    public void checkAttack() {
-//    }
-//
-//    @Test
-//    public void checkKeys() {
-//    }
-//
-//    @Test
-//    public void hit() {
-//    }
-//
-//    @Test
-//    public void update() {
-//    }
-//
-//    @Test
-//    public void checkCollect() {
-//    }
-//
-//    @Test
-//    public void draw() {
-//    }
-//
-//    @Test
-//    public void setPosition() {
-//    }
+    @Test
+    public void checkDead() {
+        player.setHealth(3);
+
+        boolean actual = player.checkDead(player);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    public void checkDeadShouldReturnTrue() {
+        player.setHealth(0);
+
+        boolean actual = player.checkDead(player);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void checkAttack() {
+        enemies = new ArrayList<Enemy>();
+        enemies.add(new Slugger(tm));
+        player.falling = true;
+
+        player.checkAttack(enemies);
+
+
+        assertEquals(1, enemies.get(0).health );
+    }
+
+    @Test
+    public void checkKeys() {
+        ArrayList<YellowKey> collectibles;
+        collectibles = new ArrayList<YellowKey>();
+        collectibles.add(new YellowKey(tm));
+
+        player.checkKeys(collectibles.get(0));
+
+        assertTrue(player.hasKey());
+        assertTrue(collectibles.get(0).isDead());
+    }
+
+    @Test
+    public void hit() {
+        enemies = new ArrayList<Enemy>();
+        enemies.add(new Slugger(tm));
+        player.falling = false;
+        player.setHealth(1);
+
+        player.hit(1);
+
+        assertEquals(0, player.getHealth());
+        assertTrue(player.isDead());
+    }
+
+
+    @Test
+    public void checkCollect() {
+        player.setCoins(0);
+        ArrayList<Collectible> goldCoins = new ArrayList<Collectible>();
+        goldCoins.add(new GoldCoin(tm));
+
+        player.checkCollect(goldCoins);
+
+        assertEquals(1, player.coins);
+    }
+
+    @Test
+    public void setPosition() {
+        player.x = 0;
+        player.y = 0;
+
+        player.setPosition(100,200);
+
+        assertEquals(100, player.getx());
+        assertEquals(200, player.gety());
+    }
 }
